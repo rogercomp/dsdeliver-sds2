03e5f03b3db3936abbbb0ffbe9852c8a9c5977ee
Rogério Siqueira de Miranda <rogercomp@gmail.com>
2021-01-08 12:50:39 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { convertTypeAcquisitionFromJson } from "typescript";
+import { OrderPayload } from "./Orders/types";
 
 const API_URL = 'http://localhost:8080';
 const mapboxToken = process.env.REACT_APP_ACESS_TOKEN_MAP_BOX;
@@ -10,4 +12,8 @@ export function fetchProducts() {
 export function fetchLocalMapBox(local: string) {
 
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayload) {
+    return axios.post(`${API_URL}/orders`, payload);
 }
